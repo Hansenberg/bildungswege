d21b15a7a55d3835e36d64ea6c4a07af3cb5fa56
avrecum <avrecum@gmail.com>
2018-05-15 16:58:48 +0200

    sql connection in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var pug = require('pug');
+var sql_connect = require ('../custom_modules/sql_connect');
+var connection = new sql_connect();
 module.exports = function(req, res){
     res.send(pug.renderFile('templates/index.pug', {
         h1:"Home"
