cd98d4ff2cac0d655d0c7aab0f82e6e43820c172
avrecum <avrecum@gmail.com>
2018-05-15 16:39:36 +0200

    added custom sql module for connecting to database

---
+++ custom_modules/sql_connect.js
@@ -0,0 +1,28 @@
+var mysql = require('mysql');
+module.exports = function (){
+    var connection = mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '',
+        db : 'bildungswege'
+    })
+    connection.connect(function(err){
+        if(err) {
+            console.log('Failed to connect to database.');
+            throw err;
+        }
+        else{
+            console.log("Successfully connected to database.");
+        }
+    })
+    connection.query("USE bildungswege;", function(err, res, fields){
+        if(err) {
+            console.log("USE bildungswege; query failed.");
+            throw err;
+        }
+        else{
+            console.log("Successfully using bildungswege");
+        }
+    })  
+    this.connection = connection;
+}
