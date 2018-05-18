39daf5b7b47b258682938fc9af810961e7470619
avrecum <avrecum@gmail.com>
2018-05-18 22:54:16 +0200

    added express middleware for parsing request body

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ var express = require('express');
 var app = express();
 var router = require('./router.js');
 app.use(router);
+app.use(express.urlencoded({
+    extended:true
+}));
 app.listen(60001);
