1df2b548bc8f06033619bc5336f3f3ad3cb60af7
avrecum <avrecum@gmail.com>
2018-05-22 16:02:19 +0200

    added static directory

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var pug = require('pug');
 var sql_connect = require ('../custom_modules/sql_connect');
 var connection = new sql_connect();
 module.exports = function(req, res){
-    res.send(pug.renderFile('templates/index.pug', {
-        h1:"Home"
+    res.send(pug.renderFile('templates/main.pug', {
+
     }));
 }
