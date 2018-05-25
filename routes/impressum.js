1c63b8be14e896d4c8b79da2d5d062739c4ac45a
avrecum <avrecum@gmail.com>
2018-05-25 18:57:28 +0200

    added impressum to pug

--- routes/impressum.js
+++ routes/impressum.js
@@ -2,14 +2,14 @@ var pug = require('pug');
 module.exports = function(req, res){
     var main = pug.compileFile('./templates/main.pug', {pretty:true});
     var main_auth = pug.compileFile('./templates/main_auth.pug');
-    var content = 'impressum';
+    var impressum = pug.compileFile('./templates/impressum.pug', {pretty:true});
     if (req.user) {
         res.send(main_auth({
-            content: content
+            content: impressum()
         }));
     } else {
         res.send(main({
-            content: content
+            content: impressum()
         }));
     }
     
