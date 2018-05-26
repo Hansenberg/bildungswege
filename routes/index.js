7af72a7971aa4251088102614acefeaf9085297c
avrecum <avrecum@gmail.com>
2018-05-26 16:52:40 +0200

    added index jumbotron and changed style

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ var pug = require('pug');
 module.exports = function(req, res){
     var main = pug.compileFile('./templates/main.pug', {pretty:true});
     var main_auth = pug.compileFile('./templates/main_auth.pug');
-    var content = 'index';
+    var index_jumbotron = pug.compileFile('./templates/index_jumbotron.pug');
     if (req.user) {
         res.send(main_auth({
-            content: content
+            content: index_jumbotron()
         }));
     } else {
         res.send(main({
-            content: content
+            content: index_jumbotron()
         }));
     }
     
