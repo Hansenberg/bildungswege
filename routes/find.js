b09d48145e7b4efd187440dab7297880a9cc02ce
avrecum <avrecum@gmail.com>
2018-06-07 09:43:07 +0200

    added find template

--- routes/find.js
+++ routes/find.js
@@ -1,16 +1,18 @@
 var pug = require('pug');
 module.exports = function(req, res){
     var main = pug.compileFile('./templates/main.pug', {pretty:true});
-    var main_auth = pug.compileFile('./templates/main_auth.pug');
-    var content = 'find';
+    var main_auth = pug.compileFile('./templates/main_auth.pug', {pretty:true});
+    var find = pug.compileFile('./templates/find.pug', {pretty:true});
+    
     if (req.user) {
-        res.send(main_auth({
-            content: content
+
+        res.send(main({
+            content: find({
+                
+            })
         }));
     } else {
-        res.send(main({
-            content: content
-        }));
+        res.redirect('/login');
     }
     
 }
