75fd9852785bf501f8fc7f29a148d807433d69ef
avrecum <avrecum@gmail.com>
2018-06-10 01:56:33 +0200

    added buggy find

--- routes/find_submit.js
+++ routes/find_submit.js
@@ -1,6 +1,7 @@
 var sql_connection = require('../custom_modules/sql_connect.js');
 var sqlcon = new sql_connection();
 var express = require('express');
+var pug = require('pug');
 
 module.exports = function(req, res){
     if(req.user){
@@ -8,26 +9,34 @@ module.exports = function(req, res){
         var weg = [];
         run_query(result);
         function run_query(id){
-        
             console.log(id)
-            sqlcon.connection.query('SELECT h.id AS id, h.haengt_ab AS haengt_ab, a.name AS name FROM abschluss a, haengt_ab_a h WHERE h.id = a.id AND h.id = ' + id + ';', function(err, res, fields){
+            sqlcon.connection.query('SELECT h.id AS id, h.haengt_ab AS haengt_ab, a.name AS name, a.min_alter AS min_alter, a.max_alter as max_alter,a.beschreibung as beschreibung, a.erreicht_durch as erreicht_durch FROM abschluss a, haengt_ab_a h WHERE h.id = a.id AND h.id = ' + id + ';', function(err, res, fields){
+                if(err) console.log(err);
                 if(res[0]){
-                    console.log(res);
-                    console.log(res.name);
                     for(i = 0; i < res.length; i++){
+                        append_to_array(res[i]);
                         run_query(res[i].haengt_ab);
                     }
                 }else{
-                    console.log();
-                    send_result()
+                    send_result();
                 }
-                
             })
         
         
         }
+        function append_to_array(res){
+            weg.push(res);
+            console.log(weg)
+        }
         function send_result(){
-            res.send('finished query. show path.')
+            var main_auth = pug.compileFile('./templates/main_auth.pug', {pretty:true});
+            var find_result = pug.compileFile('./templates/find_result.pug', {pretty:true});
+            console.log('got here');
+            res.send(main_auth({
+                content: find_result({
+                    results: weg
+                })
+            }))
         }
 
 
