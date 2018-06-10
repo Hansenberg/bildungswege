986792574a630d6e5831b8e6ee8a628bd5d852f2
avrecum <avrecum@gmail.com>
2018-06-10 15:44:38 +0200

    added functioning path trees

--- routes/find_submit.js
+++ routes/find_submit.js
@@ -1,37 +1,66 @@
 var sql_connection = require('../custom_modules/sql_connect.js');
 var sqlcon = new sql_connection();
-var express = require('express');
 var pug = require('pug');
+var path_obj = require('../models/path.js');
 
 module.exports = function(req, res){
     if(req.user){
+        var amountOfQueries =1;
         var result = req.body.goal;
         var weg = [];
-        run_query(result);
-        function run_query(id){
-            console.log(id)
-            sqlcon.connection.query('SELECT h.id AS id, h.haengt_ab AS haengt_ab, a.name AS name, a.min_alter AS min_alter, a.max_alter as max_alter,a.beschreibung as beschreibung, a.erreicht_durch as erreicht_durch FROM abschluss a, haengt_ab_a h WHERE h.id = a.id AND h.id = ' + id + ';', function(err, res, fields){
+        weg.push(new path_obj());
+        run_query(result, 0);
+        function run_query(id, pathindex){
+            sqlcon.connection.query('SELECT * FROM abschluss WHERE id= '+ id + ';', function(err, res, fields){
                 if(err) console.log(err);
-                if(res[0]){
-                    for(i = 0; i < res.length; i++){
-                        append_to_array(res[i]);
-                        run_query(res[i].haengt_ab);
-                    }
-                }else{
-                    send_result();
-                }
-            })
-        
-        
+                append_to_array(res[0], pathindex);
+                sqlcon.connection.query('SELECT haengt_ab FROM haengt_ab_a WHERE id ='+res[0].id+';', function(err, res, fields){
+                    if(err) console.log(err);
+                    handle_subqueries(res, pathindex);
+                })
+                //launch dependency queries
+
+        })
+        function wait(num){
+            amountOfQueries+=num;
+            console.log("queries"+amountOfQueries);
+            if(amountOfQueries==0){
+                send_result()
+            }
         }
-        function append_to_array(res){
-            weg.push(res);
-            console.log(weg)
+        function handle_subqueries(res, pathindex){
+            if(res[0]){
+                wait(res.length);
+                wait(-1);
+                for(i = 0; i< res.length; i++){
+                    if(i == 0){
+                        run_query(res[i].haengt_ab, pathindex);
+                    }else{
+                        var newPath = new path_obj();
+                        newPath.array = weg[pathindex].array.slice();
+                        weg.push(newPath);
+                        run_query(res[i].haengt_ab, weg.length-1);
+                    }
+                    
+                }
+                
+            }else{
+                console.log(res[0])
+                wait(-1);
+            }
+        }
+        }
+        function append_to_array(res, pathindex){
+            console.log(pathindex);
+            if(weg[pathindex]){
+                weg[pathindex].array.push(res);
+            }else{
+                throw new Error('can\'t push: path does not exist!');
+            }
         }
         function send_result(){
             var main_auth = pug.compileFile('./templates/main_auth.pug', {pretty:true});
             var find_result = pug.compileFile('./templates/find_result.pug', {pretty:true});
-            console.log('got here');
             res.send(main_auth({
                 content: find_result({
                     results: weg
@@ -43,6 +72,6 @@ module.exports = function(req, res){
 
     }
     else{
-
+        res.redirect('/login');
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@ module.exports = function(req, res){
     var main_auth = pug.compileFile('./templates/main_auth.pug');
     var index_jumbotron = pug.compileFile('./templates/index_jumbotron.pug');
     if (req.user) {
-        res.send(main_auth({
-            content: index_jumbotron()
-        }));
+        res.redirect('/find');
     } else {
         res.send(main({
             content: index_jumbotron()
